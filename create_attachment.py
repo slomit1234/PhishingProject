aa695210a8bf60259ad09cdccc891a48e0aee316
slomit1234 <42152443+slomit1234@users.noreply.github.com>
2023-06-20 12:50:38 +0300

    Delete create_attachment.py

--- create_attachment.py
+++
@@ -1,9 +0,0 @@
-def copy():
-    file = open("attachment.py", "r")
-    copy_file = open("copy_attachment.py", "w")
-    copy_file.write(fp.read())
-    file.close()
-    copy_file.close()
-
-if __name__ == "__main__":
-    copy()
